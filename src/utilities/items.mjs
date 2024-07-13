94dddfb79fc9ce4a42367232ae6c320ccfff8530
tack73 <wavetakuto460@gmail.com>
2024-07-13 14:03:58 +0900

    modified areas

--- src/utilities/items.mjs
+++ src/utilities/items.mjs
@@ -1,102 +1,102 @@
 const itemsData = [
-    {
-      "id": 1,
-      "name": "ジンジャーエール",
-      "type": "Drinks",
-      "price": 200,
-      "image": "/img/menu/Ginger_Ale.jpg",
-      "allergen": [],
-      "area" : "Drink"
-    },{
-      "id": 2,
-      "name": "アイスコーヒー",
-      "type": "Drinks",
-      "price": 200,
-      "image": "/img/menu/Orange_Juice.png",
-      "allergen": [],
-      "area" : "Drink"
-    },
-    {
-      "id": 3,
-      "name": "ホットコーヒー",
-      "type": "Drinks",
-      "price": 200,
-      "image": "/img/menu/coffee.jpg",
-      "allergen": [],
-      "area" : "Drink"
-    },
-    {
-      "id": 4,
-      "name": "紅茶",
-      "type": "Drinks",
-      "price": 200,
-      "image": "/img/menu/Black_Tea.png",
-      "allergen": [],
-      "area" : "Drink"
-    },
-    {
-      "id": 5,
-      "name": "リンゴジュース",
-      "type": "Drinks",
-      "price": 200,
-      "image": "/img/menu/Orange_Juice.png",
-      "allergen": [27],
-      "area" : "Drink"
-    },
-    {
-      "id": 6,
-      "name": "ワッフル",
-      "type": "Sweets",
-      "price": 300,
-      "image": "/img/menu/waffle.jpg",
-      "allergen": [1, 2],
-      "area" : "Waffle"
-    },
-    {
-      "id": 7,
-      "name": "ベリーパフェ",
-      "type": "Sweets",
-      "price": 400,
-      "image": "/img/menu/Parfait.jpg",
-      "allergen": [1, 2],
-      "area" : "Parfait"
-    },
-    {
-      "id": 10,
-      "name": "抹茶パフェ",
-      "type": "Sweets",
-      "price": 400,
-      "image": "/img/menu/matcha-pafait.jpg",
-      "allergen": [1, 2],
-      "area" : "Parfait"
-    },
-    {
-      "id": 9,
-      "name": "タルト",
-      "type": "Sweets",
-      "price": 400,
-      "image": "/img/menu/Cupcake.png",
-      "allergen": [1, 2],
-      "area" : "Waffle"
-    },
-    {
-      "id": 11,
-      "name": "クロックムッシュ",
-      "type": "Foods",
-      "price": 300,
-      "image": "/img/menu/QM.jpg",
-      "allergen": [4,6,7,20,21,23],
-      "area" : "Qroque_Monsieur"
-    },
-    {
-      "id": 12,
-      "name": "コンソメスープ",
-      "type": "Foods",
-      "price": 300,
-      "image": "/img/menu/consomme.jpg",
-      "allergen": [1, 2],
-      "area" : "Consomme_Soup"
-    }
-  ]
-  
+  {
+    "id": 1,
+    "name": "ジンジャーエール",
+    "type": "Drinks",
+    "price": 200,
+    "image": "/img/menu/Ginger_Ale.jpg",
+    "allergen": [],
+    "area" : "Ginger"
+  },{
+    "id": 2,
+    "name": "アイスコーヒー",
+    "type": "Drinks",
+    "price": 200,
+    "image": "/img/menu/Orange_Juice.png",
+    "allergen": [],
+    "area" : "Drink"
+  },
+  {
+    "id": 3,
+    "name": "ホットコーヒー",
+    "type": "Drinks",
+    "price": 200,
+    "image": "/img/menu/coffee.jpg",
+    "allergen": [],
+    "area" : "Coffee"
+  },
+  {
+    "id": 4,
+    "name": "紅茶",
+    "type": "Drinks",
+    "price": 200,
+    "image": "/img/menu/Black_Tea.png",
+    "allergen": [],
+    "area" : "Drink"
+  },
+  {
+    "id": 5,
+    "name": "リンゴジュース",
+    "type": "Drinks",
+    "price": 200,
+    "image": "/img/menu/Orange_Juice.png",
+    "allergen": [27],
+    "area" : "Drink"
+  },
+  {
+    "id": 6,
+    "name": "ワッフル",
+    "type": "Sweets",
+    "price": 300,
+    "image": "/img/menu/waffle.jpg",
+    "allergen": [1, 2],
+    "area" : "Waffle"
+  },
+  {
+    "id": 7,
+    "name": "ベリーパフェ",
+    "type": "Sweets",
+    "price": 400,
+    "image": "/img/menu/Parfait.jpg",
+    "allergen": [1, 2],
+    "area" : "Parfait"
+  },
+  {
+    "id": 10,
+    "name": "抹茶パフェ",
+    "type": "Sweets",
+    "price": 400,
+    "image": "/img/menu/matcha-pafait.jpg",
+    "allergen": [1, 2],
+    "area" : "Parfait"
+  },
+  {
+    "id": 9,
+    "name": "タルト",
+    "type": "Sweets",
+    "price": 400,
+    "image": "/img/menu/Cupcake.png",
+    "allergen": [1, 2],
+    "area" : "Tart"
+  },
+  {
+    "id": 11,
+    "name": "クロックムッシュ",
+    "type": "Foods",
+    "price": 300,
+    "image": "/img/menu/QM.jpg",
+    "allergen": [4,6,7,20,21,23],
+    "area" : "Qroque_Monsieur"
+  },
+  {
+    "id": 12,
+    "name": "コンソメスープ",
+    "type": "Foods",
+    "price": 300,
+    "image": "/img/menu/consomme.jpg",
+    "allergen": [1, 2],
+    "area" : "Consomme_Soup"
+  }
+]
+
 export default itemsData;
