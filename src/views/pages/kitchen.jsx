e7a8d57fdda3f749faf2b7a0b876b65ec54af856
tack73 <wavetakuto460@gmail.com>
2024-09-23 23:25:42 +0900

    modified 9090

--- src/views/pages/kitchen.jsx
+++ src/views/pages/kitchen.jsx
@@ -146,12 +146,12 @@ export default function Kitchen({ area, setArea }) {
 }
 
 function Submit9090({area,setIsPopupVisible}){
-  const endpoint9090 = "https://script.google.com/macros/s/AKfycbyp3pSDb4yRmjPfkhRbG9W4S1ZwWsSjQZD5IGKK0jBft_4s8dhfQLuFwNZm453mBfcBgg/exec";
+  const endpoint9090 = "https://script.google.com/macros/s/AKfycbyYPQoaOpuwDda2xcj1sZ2V6nmz4zRP0zchAqrPHcbBJtlEtf3yKLFb-ZbFXHe8E_S9zw/exec";
   function submit (){
     setIsPopupVisible(true);
     const today = new Date();
     const submitData = `${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-    axios.post(endpoint9090,{"name" : area, "timeStamp" : submitData}).then(()=>{setIsPopupVisible(false)});
+    axios.get(endpoint9090,{"name" : area, "timeStamp" : submitData}).then(()=>{setIsPopupVisible(false)});
   }
   return(
     <div>
