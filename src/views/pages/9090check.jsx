9593475f044b81fda31586f9f65388ba05b59754
tack73 <wavetakuto460@gmail.com>
2024-09-24 13:09:25 +0900

    9090

--- src/views/pages/9090check.jsx
+++ src/views/pages/9090check.jsx
@@ -5,57 +5,67 @@ import axios from "axios";
 import Select from "react-select";
 
 export default function Check() {
-    const [date, setDate] = useState("");
-    const endpoint = "api/status9090/";
-    const datas = axios.get(endpoint+"date");
-    let options = [];
-    datas.then((res)=>{
-        res.data.forEach((element)=>{
-            options.push({value:element,label:element});
-        });
+  const [date, setDate] = useState("");
+  const endpoint = "api/status9090/";
+  const datas = axios.get(endpoint + "date");
+  let options = [];
+  datas.then((res) => {
+    res.data.forEach((element) => {
+      options.push({ value: element, label: element });
     });
+  });
 
-    function onChange(e){
-        setDate(e.value);
-    }
-    async function Status9090view({area,date}){
-        let data = [];
-        await axios.get(endpoint,{params:{name:area,time:date}}).then((res)=>{
-            data = res.data;
-        });
-        return (
-            <div>
-                <h1>{area}</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ローテ回数</th>
-                            <th>時間</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((item) => (
-                            <tr>
-                                <td>{item.index}</td>
-                                <td>{item.time}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+  function onChange(e) {
+    setDate(e.value);
+  }
+  async function Status9090view({ area, date }) {
+    // let data = [];
+    // await axios
+    //   .get(endpoint, { params: { name: area, time: date } })
+    //   .then((res) => {
+    //     data = res.data;
+    //   });
+    return (
+      <div>
+        <h1>{area}</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>ローテ回数</th>
+              <th>時間</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {data.map((item) => (
+              <tr>
+                <td>{item.index}</td>
+                <td>{item.time}</td>
+              </tr>
+            ))} */}
+            <tr>
+                <td>1</td>
+                <td>2021/9/1</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 
-
-    return(
-        <div>
-            <h1>9090 Check</h1>
-            <Select options={options} isSearchable={true} className={styles.selectView} onChange={onChange}/>
-            <Status9090view area="Tart" date={date}/>
-            <Status9090view area="Waffle" date={date}/>
-            <Status9090view area="Ginger" date={date}/>
-            <Status9090view area="Consomme_Soup" date={date}/>
-            <Link to="/">Back to Home</Link>
-        </div>
-    )
+  return (
+    <div>
+      <h1>9090 Check</h1>
+      <Select
+        options={options}
+        isSearchable={true}
+        className={styles.selectView}
+        onChange={onChange}
+      />
+      <Status9090view area="Tart" date={date} />
+      <Status9090view area="Waffle" date={date} />
+      <Status9090view area="Ginger" date={date} />
+      <Status9090view area="Consomme_Soup" date={date} />
+      <Link to="/">Back to Home</Link>
+    </div>
+  );
 }
