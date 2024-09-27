8d275221cd756831738bf0657c930cd4b46b6a54
tack73 <wavetakuto460@gmail.com>
2024-09-28 01:02:38 +0900

    9090check: add 9090check page

---
+++ src/views/components/modules/View9090.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import styles from "./9090check.module.css";
+import axios from "axios";
+import * as Realm from "realm-web";
+const app = new Realm.App({ id: "application-0-vmbzlrz" });
+
+export default function Check9090({ area, date }) {
+  const [user, setUser] = useState(null);
+  const [events, setEvents] = useState([]);
+  const [data, setData] = useState([]);
+  const endpoint = "api/status9090/";
+  function getStatus9090({ area, date }) {
+    axios
+      .get(endpoint, { params: { name: area, time: date } })
+      .then(function (response) {
+        setData(response.data);
+      });
+  }
+  useEffect(() => {
+    const login = async () => {
+      const user = await app.logIn(Realm.Credentials.anonymous());
+      setUser(user); // Connect to the database
+      getStatus9090({ area: area, date: date });
+
+      const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+      const collection = mongodb.db("test").collection("status9090"); // Everytime a change happens in the stream, add it to the list of events
+
+      for await (const change of collection.watch()) {
+        setEvents((events) => [...events, change]);
+        getStatus9090({ area: area, date: date });
+      }
+    };
+    login();
+  }, []);
+
+  return (
+    <div>
+      <h1>{area}</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ローテ回数</th>
+            <th>時間</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr>
+              <td>{item.index}</td>
+              <td>{item.time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- src/views/pages/9090check.jsx
+++ src/views/pages/9090check.jsx
@@ -3,55 +3,52 @@ import React, { useState, useEffect } from "react";
 import styles from "./9090check.module.css";
 import axios from "axios";
 import Select from "react-select";
+import * as Realm from "realm-web";
+import View9090 from "../components/modules/View9090.jsx";
+
+const app = new Realm.App({ id: "application-0-vmbzlrz" });
+
+
+
+
 
 export default function Check() {
-  const [date, setDate] = useState("");
-  const [data, setData] = useState([]);
+  const [user, setUser] = useState(null);
+  const [events, setEvents] = useState([]);
+  const [date, setDate] = useState("日付を選択");
+  const [options, setOptions] = useState([]);
   const endpoint = "api/status9090/";
-  let options = [];
-  useEffect(() => {
-    const datas = axios.get(endpoint + "date");
-    datas.then((res) => {
-      res.data.forEach((element) => {
-        options.push({ value: element, label: element });
-      });
+  function getDates() {
+    axios.get(endpoint + "date").then(function (response) {
+      setOptions(
+        response.data.map((date) => {
+          return { value: date, label: date };
+        })
+      );
     });
-  });
+  }
+  useEffect(() => {
+    const login = async () => {
+      const user = await app.logIn(Realm.Credentials.anonymous());
+      setUser(user); // Connect to the database
+      getDates();
+
+      const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+      const collection = mongodb.db("test").collection("status9090"); // Everytime a change happens in the stream, add it to the list of events
+
+      for await (const change of collection.watch()) {
+        setEvents((events) => [...events, change]);
+        getDates();
+      }
+    };
+    login();
+  }, []);
   
   
 
   function onChange(e) {
     setDate(e.value);
   }
-  function Status9090view({ area, date, data, setData }) {
-    axios
-      .get(endpoint, { params: { name: area, time: date } })
-      .then((res) => {
-        setData(res.data);
-      });
-    return (
-      <div>
-        <h1>{area}</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>ローテ回数</th>
-              <th>時間</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => (
-              <tr>
-                <td>{item.index}</td>
-                <td>{item.time}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-
   return (
     <div>
       <h1>9090 Check</h1>
@@ -61,10 +58,10 @@ export default function Check() {
         className={styles.selectView}
         onChange={onChange}
       />
-      <Status9090view area="Tart" date={date} data={data} setData={setData} />
-      <Status9090view area="Waffle" date={date} data={data} setData={setData} />
-      <Status9090view area="Ginger" date={date} data={data} setData={setData} />
-      <Status9090view area="Consomme_Soup" date={date} data={data} setData={setData} />
+      <View9090 area="Tart" date={date} />
+      <View9090 area="Waffle" date={date} />
+      <View9090 area="Ginger" date={date} />
+      <View9090 area="Consomme_Soup" date={date} />
       <Link to="/">Back to Home</Link>
     </div>
   );
